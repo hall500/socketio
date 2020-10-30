7b009cd41de48f516528b4a997d9d7260ac06998
repl.it user <replituser@example.com>
2020-10-30 22:40:10 +0000

    Socket IO updated to connet chat users

--- public/client.js
+++ public/client.js
@@ -2,12 +2,28 @@ $( document ).ready(function() {
   
   /*global io*/
 var socket = io();
+socket.on('user', data => {
+  $('#num-users').text(data.currentUsers + ' users online');
+  let message =
+    data.name +
+    (data.connected ? ' has joined the chat.' : ' has left the chat.');
+  $('#messages').append($('<li>').html('<b>' + message + '</b>'));
+});
+
+socket.on("chat message", data => {
+  let message =
+    data.name +
+    ": " + data.message;
+  $('#messages').append($('<li>').html(
+    '<b>' + message + '</b>'));
+});
 
    
   // Form submittion with new message in field with id 'm'
   $('form').submit(function(){
     var messageToSend = $('#m').val();
     //send message to server here?
+    socket.emit("chat message", messageToSend);
     $('#m').val('');
     return false; // prevent form submit from refreshing page
   });
--- server.js
+++ server.js
@@ -8,15 +8,31 @@ const auth        = require('./app/auth.js');
 const routes      = require('./app/routes.js');
 const mongo       = require('mongodb').MongoClient;
 const passport    = require('passport');
-const cookieParser= require('cookie-parser')
+const passportSocketIo = require("passport.socketio");
+const cookieParser= require('cookie-parser');
+const MongoStore = require("connect-mongo")(session);
 const app         = express();
 const cors = require('cors');
-app.use(cors());
 const http        = require('http').Server(app);
 const sessionStore= new session.MemoryStore();
 const io = require('socket.io')(http);
 
+app.use(cors());
+
+const URI = process.env.DATABASE;
+const store = new MongoStore({ url: URI });
+
 fccTesting(app); //For FCC testing purposes
+
+function onAuthorizeSuccess(data, accept){
+  console.log("Successful connection to socket.io");
+}
+
+function onAuthorizeFail(data, message, error, accept){
+  if(error) throw new Error(message);
+  console.log("failed connection to socket.io", message);
+  accept(null, false);
+}
 
 app.use('/public', express.static(process.cwd() + '/public'));
 app.use(cookieParser());
@@ -32,6 +48,17 @@ app.use(session({
   store: sessionStore,
 }));
 
+io.use(
+  passportSocketIo.authorize({
+    cookieParser: cookieParser,
+    key: "express.sid",
+    secret: process.env.SESSION_SECRET,
+    store: store,
+    success: onAuthorizeSuccess,
+    fail: onAuthorizeFail
+  })
+);
+
 
 mongo.connect(process.env.DATABASE, (err, dbo) => {
     const db = dbo.db("socketio");
@@ -42,10 +69,33 @@ mongo.connect(process.env.DATABASE, (err, dbo) => {
       
     http.listen(process.env.PORT || 3000);
 
-  
+    let currentUsers = 0;
     //start socket.io code  
     io.on("connection", socket => {
+      ++currentUsers;
+      io.emit("user", {
+        name: socket.request.user.name,
+        currentUsers,
+        connected: true
+      });
       console.log('A user has connected');
+
+      socket.on("chat message", function(message){
+        io.emit("chat message", {
+          name: socket.request.user.name,
+          message
+        })
+      });
+
+      socket.on("disconnect", function(data){
+        console.log("A user has disconnected");
+        --currentUsers;
+        io.emit("user", {
+          name: socket.request.user.name,
+          currentUsers,
+          connected: false
+        });
+      });
     });
   
 
